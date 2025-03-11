0cc91d1eab5876c5e2af347865468fd76db3564c
yukina2024 <o9o258oo43q@outlook.jp>
2025-03-11 23:58:41 +0900

    Fix: Adjust script path and vite base setting

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // ✅ これを追加
+  base: "/", // ✅ これを追加
   plugins: [react()],
   build: {
     outDir: "dist",
